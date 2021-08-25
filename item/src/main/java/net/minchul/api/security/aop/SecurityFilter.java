7432307431779fa4effb1d59520c11cee4d287d4
MinChul-Son <smc5236@naver.com>
2021-08-25 18:29:23 +0900

    Spring Security : 설정
    - JWT 토큰을 제공하는 provider
    - 이를 검증하는 Filter
    - url 접근 권한을 설정하는 config
    - 그외에 예외를 처리하는 Handler들

--- item/src/main/java/net/minchul/api/security/aop/SecurityFilter.java
+++ item/src/main/java/net/minchul/api/security/aop/SecurityFilter.java
@@ -13,10 +13,15 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class SecurityFilter extends OncePerRequestFilter {
 
-    private final SecurityProvider provider;
+    /**
+     *  provider -> 서버쪽에서 jwt 토큰을 발급해서 프론트로 전달해줌. 이를 통해 통신
+     *  filter를 통해 jwt 토큰이 맞는지 검증
+     */
+
+    private final SecurityProvider provider; 
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-
+//        String token = provider
     }
 }
---
+++ item/src/main/java/net/minchul/api/security/domain/Role.java
@@ -0,0 +1,7 @@
+package net.minchul.api.security.domain;
+
+public class Role {
+    public String getAuthority() {
+        return "";
+    }
+}
