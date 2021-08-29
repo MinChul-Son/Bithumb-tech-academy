581f17af3b7ad2c5cc4f45f0553ae541c761c152
MinChul-Son <smc5236@naver.com>
2021-08-29 18:58:02 +0900

    feature : UserDetailsServiceImpl
    - UserDetailsService를 구현

--- item/src/main/java/net/minchul/api/security/domain/UserDetailsServiceImpl.java
+++ item/src/main/java/net/minchul/api/security/domain/UserDetailsServiceImpl.java
@@ -1,9 +1,25 @@
 package net.minchul.api.security.domain;
 
+import lombok.RequiredArgsConstructor;
+import net.minchul.api.user.domain.User;
+import net.minchul.api.user.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-public class UserDetailsServiceImpl {
-    public UserDetails loadUserByUsername(String username) {
-        return null;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = Optional.ofNullable(userRepository.findByUsername(username))
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found!"));
+        return UserDetailsImpl.build(user.get());
     }
 }
