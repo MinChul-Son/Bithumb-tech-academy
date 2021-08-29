ee56a43a7860ce228413d9378535e9dd99facb5d
MinChul-Son <smc5236@naver.com>
2021-08-29 18:56:40 +0900

    feature : UserController
    - 회원가입, 로그인

--- item/src/main/java/net/minchul/api/user/controller/UserController.java
+++ item/src/main/java/net/minchul/api/user/controller/UserController.java
@@ -1,14 +1,47 @@
 package net.minchul.api.user.controller;
 
+import io.swagger.annotations.*;
+import lombok.RequiredArgsConstructor;
 import net.minchul.api.user.domain.User;
+import net.minchul.api.user.domain.UserDto;
+import net.minchul.api.user.service.UserServiceImpl;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@Api(tags = "users")
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    private final UserServiceImpl userService;
+    private final ModelMapper modelMapper;
+
+    @PostMapping("/signup")
+    @ApiOperation(value = "${UserController.signUp}")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "something wrong"),
+            @ApiResponse(code = 403, message = "승인거절"),
+            @ApiResponse(code = 422, message = "중복된 username")})
+    public ResponseEntity<String> singUp(@ApiParam("SignUp User")
+                                         @RequestBody UserDto userDto) {
+        return ResponseEntity.ok(userService.signUp(modelMapper.map(userDto, User.class)));
+    }
+
+    @PostMapping("/signin")
+    @ApiOperation(value = "${UserController.signIn}")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "something wrong"),
+            @ApiResponse(code = 422, message = "유효하지 않은 아이디 / 비밀번호")})
+    public ResponseEntity<UserDto> singIn(@ApiParam("SignIn User")
+                                         @RequestBody UserDto userDto) {
+        return ResponseEntity.ok(userService.signIn(modelMapper.map(userDto, User.class)));
+    }
+
     @GetMapping
     public List<User> findAll() {
         return null;
