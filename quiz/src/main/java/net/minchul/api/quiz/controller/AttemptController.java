fd61c0de1210e9d5b7665ede3c60aaa89bcc9cdd
MinChul-Son <smc5236@naver.com>
2021-09-01 21:28:00 +0900

    feature : 퀴즈 맞추기

--- quiz/src/main/java/net/minchul/api/quiz/controller/AttemptController.java
+++ quiz/src/main/java/net/minchul/api/quiz/controller/AttemptController.java
@@ -1,4 +1,42 @@
 package net.minchul.api.quiz.controller;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import net.minchul.api.quiz.domain.Attempt;
+import net.minchul.api.quiz.service.QuizService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/results")
 public class AttemptController {
+    private final QuizService quizService;
+
+    @PostMapping
+    ResponseEntity<Attempt> postResult(@RequestBody Attempt attempt) {
+        boolean isCorrect = quizService.checkAttempt(attempt);
+        return ResponseEntity.ok(new Attempt(attempt.getUser(), attempt.getQuiz(), attempt.getResultAttempt(), isCorrect));
+    }
+
+    @GetMapping
+    ResponseEntity<Flux<Attempt>> getStatistics(@RequestParam("alias") String alias) {
+        return ResponseEntity.ok(quizService.getStatsForUser(alias));
+    }
+
+    @GetMapping("/{id}")
+    ResponseEntity<Mono<Attempt>> getResultById(final @PathVariable("id") long id) {
+        return ResponseEntity.ok(quizService.getResultById(id));
+    }
+
+
+    @RequiredArgsConstructor
+    @NoArgsConstructor(force = true)
+    @Getter
+    static final class ResultResponse {
+        private final boolean correct;
+    }
 }
--- quiz/src/main/java/net/minchul/api/quiz/service/QuizService.java
+++ quiz/src/main/java/net/minchul/api/quiz/service/QuizService.java
@@ -1,20 +1,46 @@
 package net.minchul.api.quiz.service;
 
 import lombok.RequiredArgsConstructor;
+import net.minchul.api.quiz.domain.Attempt;
 import net.minchul.api.quiz.domain.Quiz;
+import net.minchul.api.quiz.domain.User;
+import net.minchul.api.quiz.repository.AttemptRepository;
+import net.minchul.api.quiz.repository.UserRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class QuizService {
-
     private final GeneratorService generatorService;
+    private final UserRepository userRepository;
+    private final AttemptRepository attemptRepository;
 
     public Mono<Quiz> createQuiz() {
         int factorA = generatorService.randomFactor();
         int factorB = generatorService.randomFactor();
         Quiz quiz = new Quiz(factorA, factorB);
         return Mono.just(quiz);
+    }
+
+    public boolean checkAttempt(Attempt attempt) {
+        // 존재하는 회원인지 체크
+        Optional<User> user = userRepository.findByAlias(attempt.getUser().getAlias());
+        Assert.isTrue(!attempt.isCorrect(), "Unable to send in graded state");
+        boolean isCorrect = attempt.getResultAttempt() == attempt.getQuiz().getFactorA() * attempt.getQuiz().getFactorB();
+        attemptRepository.save(new Attempt(attempt.getUser(), attempt.getQuiz(), attempt.getResultAttempt(), isCorrect));
+        return isCorrect;
+    }
+
+    public Flux<Attempt> getStatsForUser(String alias) {
+        return attemptRepository.findTop5ByUserAliasOrderByIdDesc(alias);
+    }
+
+    public Mono<Attempt> getResultById(long id) {
+        return null;
     }
 }
