7e7b2e253727e11ec71785c64f0a6b48531898fd
MinChul-Son <smc5236@naver.com>
2021-09-01 18:53:40 +0900

    feature : Producers, Consumers

---
+++ quiz/src/main/java/net/minchul/api/quiz/domain/Consumers.java
@@ -0,0 +1,17 @@
+package net.minchul.api.quiz.domain;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class Consumers {
+
+    @KafkaListener(topics = "kafka-test")
+    public void listenGroup(String message) {
+        log.info("Received Message :{}", message);
+    }
+}
