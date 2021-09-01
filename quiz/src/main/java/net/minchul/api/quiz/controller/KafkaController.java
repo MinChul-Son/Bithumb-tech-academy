e679159dcd940acc217b2e913aa8029bc64659ea
MinChul-Son <smc5236@naver.com>
2021-09-01 19:05:11 +0900

    feature : 메시지 전송, 수신
    - Producers에서 메시지 전송
    - Consumers에서 메시지 수신, 로그로 찍음

--- quiz/src/main/java/net/minchul/api/quiz/controller/KafkaController.java
+++ quiz/src/main/java/net/minchul/api/quiz/controller/KafkaController.java
@@ -2,6 +2,7 @@ package net.minchul.api.quiz.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.minchul.api.quiz.domain.Producers;
 import net.minchul.api.quiz.service.KafkaSender;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class KafkaController {
     private final KafkaSender kafkaSender;
+    private final Producers producers;
 
     @GetMapping
     public String hello() {
@@ -25,5 +27,10 @@ public class KafkaController {
         log.info("##### Producer Request #####");
         kafkaSender.send(message);
         return "Message Sent to Kafka Success!";
+    }
+
+    @GetMapping("/receiver")
+    public void receiver() {
+        producers.sendMessage("kafka-test", "I love Spring");
     }
 }
