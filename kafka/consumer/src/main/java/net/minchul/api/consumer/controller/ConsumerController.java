219f2ec4c2ebf6f76bcfa764bf47788f0e177fb2
MinChul-Son <smc5236@naver.com>
2021-09-08 22:53:00 +0900

    feature : 도메인, 컨트롤러 생성

---
+++ kafka/consumer/src/main/java/net/minchul/api/consumer/controller/ConsumerController.java
@@ -0,0 +1,16 @@
+package net.minchul.api.consumer.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/kafka")
+public class ConsumerController {
+
+    @GetMapping
+    public String home(){
+        return "Hello Kafka";
+    }
+}
---
+++ kafka/consumer/src/main/java/net/minchul/api/consumer/domain/User.java
@@ -0,0 +1,20 @@
+package net.minchul.api.consumer.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data @Component
+public class User {
+    private String id;
+    private String name;
+    private String email;
+
+    @Override
+    public String toString() {
+        return String.format("%s, %s, %s \n", id, name, email);
+    }
+}
